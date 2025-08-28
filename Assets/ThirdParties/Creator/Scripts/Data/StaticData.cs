40a1a57398fa7a45889e1607971a16323ccb8736
WINDOWS-PC\datnt <thedat071298@gmail.com>
2025-08-28 14:42:17 +0700

    review code

--- Assets/ThirdParties/Creator/Scripts/Data/StaticData.cs
+++ Assets/ThirdParties/Creator/Scripts/Data/StaticData.cs
@@ -36,63 +36,6 @@ public static class StaticData
             return RemoteConfigController.GetFloatConfig("af_purchase_manual", 0.7f);
         }
     }
-
-    public static int RemoveAdFrequency
-    {
-        get
-        {
-            return RemoteConfigController.GetIntConfig("remove_ad_frequency", 10);
-        }
-    }
-
-    public static int RemoveAdFirst
-    {
-        get
-        {
-            return RemoteConfigController.GetIntConfig("remove_ad_first", 10);
-        }
-    }
-
-    public static bool RwBooster
-    {
-        get
-        {
-            return RemoteConfigController.GetBoolConfig("rw_booster", true);
-        }
-    }
-
-    public static bool RwBoosterHide
-    {
-        get
-        {
-            return RemoteConfigController.GetBoolConfig("rw_booster_hide", true);
-        }
-    }
-
-    public static bool LoseInterAd
-    {
-        get
-        {
-            return RemoteConfigController.GetBoolConfig("lose_interAd", true);
-        }
-    }
-
-    public static int BoosterAvailable
-    {
-        get
-        {
-            return RemoteConfigController.GetIntConfig("booster_available", 3);
-        }
-    }
-
-    public static int AmountRevive
-    {
-        get
-        {
-            return RemoteConfigController.GetIntConfig("amount_revive", 8);
-        }
-    }
-
     public static int MaxAdsSpins
     {
         get
